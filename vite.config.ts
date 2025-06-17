be06c3033edb451625a2a93c14366b7c9b2b508f
KarlaSz <szymaszkiewiczk@gmail.com>
2025-06-17 17:00:04 +0200

    pl translation proejct

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: '/',
+  base: '/webszyk-brand/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
